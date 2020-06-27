8d678b8325f7541a8d70823cb0f5930f6e58e7f6
nckappdev <thatdeveloperguy1301@gmail.com>
2020-06-27 21:26:23 +0100

    share

--- app/src/main/java/com/example/databindingfragment/Third.kt
+++ app/src/main/java/com/example/databindingfragment/Third.kt
@@ -1,12 +1,13 @@
 package com.example.databindingfragment
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
+import androidx.core.app.ShareCompat
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import com.example.databindingfragment.databinding.FragmentThirdBinding
 
@@ -19,10 +20,22 @@ class Third : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding =  DataBindingUtil.inflate<FragmentThirdBinding>(inflater, R.layout.fragment_third, container, false)
+
+
+        setHasOptionsMenu(true)
+
+
         binding.playAgain.setOnClickListener {
             it.findNavController().navigate(R.id.action_third_to_secondFragmnet)
 
         }
+
+
+
+
+
+
+
         
         var args = ThirdArgs.fromBundle(requireArguments())
 
@@ -33,6 +46,52 @@ class Third : Fragment() {
 
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.winner, menu)
+
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)){
+           menu.findItem(R.id.share).setVisible(false)
+        }
+
+
+
+    }
+
+
+    private  fun getShareIntent():Intent {
+        var args = ThirdArgs.fromBundle(requireArguments())
+
+        return ShareCompat.IntentBuilder.from(requireActivity())
+            .setText(getString(R.string.share_success_text, args.numCorrect))
+            .setType("text/plain")
+            .intent
+
+//        var shareIntent = Intent(Intent.ACTION_SEND)
+//        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect))
+//        return  shareIntent
+    }
+
+
+
+//LONG WAY
+        private fun shareSuccess(){
+            startActivity(getShareIntent())
+        }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item!!.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
 
 
 }
